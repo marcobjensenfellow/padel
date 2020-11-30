ecf1fb3ce2d8f26ed954ef68d2204b212f044ade
Anton Ödman <anton.odman@wow-group.com>
2020-11-30 09:45:56 +0100

    Added store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,8 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import americanoStore from "./modules/americanoStore";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    modules: {
+        americano: americanoStore,
+    },
+});
