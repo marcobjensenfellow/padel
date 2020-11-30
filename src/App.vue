9fdec5d7e9e5fa105192c1097e5f1695ef64f7ef
Anton Ödman <anton.odman@wow-group.com>
2020-11-30 09:52:09 +0100

    Added basics

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Nav></Nav>
+  <router-view />
 </template>
+
+<script>
+import Nav from "./views/Nav.vue";
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/views/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link class="nav-item nav-link" to="/">Americano</router-link>
+        <router-link class="nav-item nav-link" to="/about">About</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
