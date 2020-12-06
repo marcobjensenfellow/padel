08f11c221f50b0b64e23d914abb99c79b0b5481c
Anton Ödman <anton.odman@gmail.com>
2020-12-06 19:10:47 +0100

    Title update

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark navbar-pdl">
-    <a class="navbar-brand" href="#">Padel Nu</a>
+    <a class="navbar-brand" href="#">PADEL NU</a>
     <button
       class="navbar-toggler"
       type="button"
