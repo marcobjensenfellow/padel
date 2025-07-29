1bbc03878ae6a53e57516c337af7d0055bc390ed
Marco Bjørslev Jensen <mbj@fellow.dk>
2025-07-29 15:26:33 +0200

    chore: upgrade tooling and remove firebase

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
-import { analytics } from "./firebase";
-
 createApp(App)
     .use(store.original)
     .use(router)
