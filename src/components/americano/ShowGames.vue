be8287ab8392da4470e8e553ca4e858b7a6dcd25
Marco Bjørslev Jensen <mbj@fellow.dk>
2025-07-30 15:42:06 +0200

    Revert "Add match scoring overlay"

--- src/components/americano/ShowGames.vue
+++ src/components/americano/ShowGames.vue
@@ -5,7 +5,10 @@
             <div class="form-group">
                 <div class="score-container">
                     <div v-for="(game, index) in getGames" :key="game.id">
-                        <div v-if="IsNewRound(index)" class="score-round">
+                        <div
+                            v-if="IsNewRound(index)"
+                            class="score-round"
+                        >
                             Round: {{ game.round }}
                         </div>
                         <div
@@ -20,20 +23,38 @@
                                 v-if="game.players.length === 4"
                                 class="d-flex flex-column flex-md-row justify-content-between"
                             >
+                                <div class="team-element p-2">
+                                    <span class="team">{{
+                                        getPlayerNames(game, 1)
+                                    }}</span>
+                                    <span class="vs"> vs </span>
+                                    <span class="team">{{
+                                        getPlayerNames(game, 2)
+                                    }}</span>
+                                </div>
+
                                 <span class="team-element pt-2">
-                                    {{ printCourt(game, index) }}
-                                </span>
-                                <MatchCourt
-                                    :game="game"
-                                    :homeName="getPlayerNames(game, 1)"
-                                    :awayName="getPlayerNames(game, 2)"
-                                    :maxPoints="getRules.maxScore"
-                                    @set-score="updateScore"
-                                />
+                                    {{ printCourt(game, index) }}</span
+                                >
+
+                                <div class="team-element p-2 align-self-center">
+                                    <input
+                                        v-model="game.homeScore"
+                                        class="input-element"
+                                        required
+                                        @focusout="handleFocusChange(game, 1)"
+                                    />
+                                    -
+                                    <input
+                                        v-model="game.awayScore"
+                                        class="input-element"
+                                        required
+                                        @focusout="handleFocusChange(game, 2)"
+                                    />
+                                </div>
                             </div>
                             <div v-else class="p-2">
-                                Rest round:
-                                {{
+                                Rest round: {{
                                     getPlayerNameById(game.players[0].playerId)
                                 }}
                             </div>
@@ -73,6 +94,7 @@ import { defineComponent } from "vue";
 import store from "@/store/index";
 import { PadelGame } from "@/models/padelGame.interface";
 import { getFullPlayerNames } from "@/services/htmlHelperService";
+import { evenScore, removeNotNumbers } from "@/services/scoreService";
 import { GameSide } from "@/models/gameSide.enum";
 
 export default defineComponent({
@@ -101,13 +123,18 @@ export default defineComponent({
         reset(): void {
             store.commit.americanoStore.RESET();
         },
-        updateScore(payload: {
-            game: PadelGame;
-            homeScore: number;
-            awayScore: number;
-        }) {
-            payload.game.homeScore = payload.homeScore;
-            payload.game.awayScore = payload.awayScore;
+        handleFocusChange(game: PadelGame, side: GameSide) {
+            removeNotNumbers(
+                game,
+                side,
+                store.getters.americanoStore.getRules.maxScore
+            );
+            evenScore(
+                game,
+                store.getters.americanoStore.getRules.maxScore,
+                side
+            );
+            store.dispatch.americanoStore.saveStateManually();
         },
         getColorCodeGroup(game: PadelGame) {
             if (store.getters.americanoStore.getRules.colorCode === false) {
